32dd8667a346802cc8afc5ccb201935bc800e936
lida lopez <linda.lucia15@hotmail.com>
2022-01-25 18:16:23 -0500

    first commit

---
+++ src/components/Formulario.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+const Formulario = () => {
+  return <div >
+      <h1 style={{fontSize: 'xx-large', textAlign: 'center'}} ><b>Nada como una Guajolota para empezar el día</b></h1>
+      
+  </div>;
+};
+
+export default Formulario;
---
+++ src/components/Usuario.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const usuario = () => {
+  return <div>
+      <nav class="navbar navbar-light ">
+<div class="container-fluid">
+<a class="navbar-brand"><img src="logo.svg" width="60px" alt=""></img></a>
+<form class="d-flex">
+<img src="https://cdn-icons.flaticon.com/png/512/2366/premium/2366367.png?token=exp=1642868532~hmac=6dd18ca8bd0e6a91785dd9e272ecb4e8" alt="" width="30px"></img>
+</form>
+</div>
+</nav>
+  </div>;
+};
+
+export default usuario;
